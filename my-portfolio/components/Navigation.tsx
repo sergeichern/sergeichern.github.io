0dec19bcb713b88947829faf2230f98607347434
sergeichern <sergeiche01@gmail.com>
2024-04-30 22:13:10 -0400

    Update Navigation.tsx

--- my-portfolio/components/Navigation.tsx
+++ my-portfolio/components/Navigation.tsx
@@ -1,51 +1,49 @@
-"use client"
-import { NavLinks } from '@/constants'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
-import Transition from './Transition'
-
+"use client";
+import { NavLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import Transition from "./Transition";
 
 const Navigation = () => {
-    const [isRouting, setisRouting] = useState(false)
-    const path = usePathname()
-    const [isActive, setisActive] = useState(path)
-    const [prevPath, setPrevPath] = useState("/")
+  const [isRouting, setisRouting] = useState(false);
+  const path = usePathname();
+  const [prevPath, setPrevPath] = useState("/");
 
-    useEffect(()=>{
-        if(prevPath !== path){
-            setisRouting(true);
-        }
-    }, [path, prevPath]);
+  useEffect(() => {
+    if (prevPath !== path) {
+      setisRouting(true);
+    }
+  }, [path, prevPath]);
 
-    useEffect(()=>{
-        if(isRouting){
-            setPrevPath(path);
-            const timeout = setTimeout(() => {
-                setisRouting(false);
-            }, 1200);
+  useEffect(() => {
+    if (isRouting) {
+      setPrevPath(path);
+      const timeout = setTimeout(() => {
+        setisRouting(false);
+      }, 1200);
 
-            return() => clearTimeout(timeout);
-        }
-    }, [isRouting]);
+      return () => clearTimeout(timeout);
+    }
+  }, [isRouting]);
   return (
-<div 
-    style={{left: "20%"}}
-    className="absolute z-[50] -bottom-20 w-[50%] md:w-[20%] max-h-[150px] rounded-full flex justify-between items-center border  border-white px-4 py-7">
-    
-    {isRouting && <Transition />}
+    <div
+      style={{ left: "20%" }}
+      className="absolute z-[50] -bottom-20 w-[50%] md:w-[20%] max-h-[150px] rounded-full flex justify-between items-center border border-white px-4 py-7"
+    >
 
-    {NavLinks.map((nav)=>(
-        <Link
-        key={nav.name}
-        href={nav.link}
-        className='mb-16 pl-4 min-w-[20%]'
-        >
-            <nav.icon className={`w-[24px] h-[24px] ${path === nav.name ? "text-orange-700": "text-white"}`}/>
+      {isRouting && <Transition />}
+      {NavLinks.map((nav) => (
+        <Link key={nav.name} href={nav.link} className="mb-16 pl-4 min-w-[20%]">
+          <nav.icon
+            className={`w-[24px] h-[24px] ${
+              path === nav.name ? "text-orange-700" : "text-white"
+            }`}
+          />
         </Link>
-    ))}
-</div>
-  )
+      ))}
+    </div>
+  );
 };
 
-export default Navigation
+export default Navigation;
